3de4bf808ea4c39d256f2ed6696388563f6c95bb
valrus <imccowan@gmail.com>
2016-06-05 10:31:43 -0700

    add some stubs I plan not to use because I want to try Firebase instead

--- src/otherfoot.py
+++ src/otherfoot.py
@@ -1,18 +1,49 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import *
 
+import json
 import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+from flask import Flask, jsonify, request
 
-def set_up_parser():
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=ArgumentDefaultsHelpFormatter)
-    return parser
+APP = Flask(__name__)
+
+ROOMS = {}
 
 
-def main():
-    args = set_up_parser().parse_args()
+class OtherfootGame(object):
+    def __init__(self, players):
+        self.cards = []
+        self.players = players
 
-    if __name__ == "__main__":
-        sys.exit(main())
+
+@app.route('/submitcard', methods=['POST'])
+def submit_card():
+    pass
+
+
+# Rooms, joining and starting games
+
+
+@app.route('/getrooms', methods=['GET'])
+def get_rooms():
+    return jsonify(ROOMS)
+
+
+@app.route('/join', methods=['POST'])
+def join_room():
+    player = request.args.get('player')
+    room = request.args.get('room')
+    if not room in ROOMS:
+        # fail
+        pass
+
+
+@app.route('/start', methods=['POST'])
+def start_game():
+    pass
+
+
+if __name__ == "__main__":
+    APP.run(debug=True)
